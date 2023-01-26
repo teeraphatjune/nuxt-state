2d3861f537d94d194f560bc61d34a41e2ea41b53
teeraphatjune <69952231+teeraphatjune@users.noreply.github.com>
2023-01-26 14:55:08 +0700

    Update FormInputs.vue

--- src/components/FormInputs.vue
+++ src/components/FormInputs.vue
@@ -12,9 +12,6 @@
           aria-describedby="emailHelp"
           placeholder="Firstname"
         />
-        <small id="emailHelp" class="form-text text-muted"
-          >We'll never share your email with anyone else.</small
-        >
       </div>
       <div class="form-group">
         <label for="lastName">LastName</label>
