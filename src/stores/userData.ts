a8377ffe239e5c0ee6de9f908d21943e8f6fa521
teeraphatjune <69952231+teeraphatjune@users.noreply.github.com>
2023-01-26 12:20:54 +0700

    setup store

--- src/stores/userData.ts
+++ src/stores/userData.ts
@@ -1,35 +1,37 @@
 import { defineStore } from "pinia";
 import { v4 as uuidv4 } from "uuid";
+import { Ref } from "vue";
 interface userData {
   id: string;
   firstName?: string;
   lastName?: string;
   comment?: string;
 }
-export const useUserDataStore = defineStore('userData', {
-  state: () => ({
-    Items: <userData[]>[],
-    test: ref('test'),
-  }),
-  getters: {
-    getTest: (state): string => state.test, // get normal text
-    getItems: (state): userData[] => state.Items,
-  },
-  actions: {
-    addItem(firstname: string, lastname: string, comment: string) : void {
-      this.Items.push(<userData>{
+
+export const useUserDataStore = defineStore("userData", () => {
+  const items = ref(<userData[]>[]);
+  const test = ref('test text');
+
+  const getItems = computed(() => items.value)
+  const getTest = computed(() => test.value)
+
+  const addItem = (firstname: string, lastname: string, comment: string): void => {
+      items.value.push(<userData>{
         id: uuidv4(),
         firstName: firstname,
         lastName: lastname,
         comment: comment,
       });
-    },
-    deleteItem(id: string) : void{
-      const index = this.Items.findIndex(item => item.id === id);
+    };
+
+  const deleteItem = (id: string): void => {
+    const index = items.value.findIndex(item => item.id === id);
       if (index > -1) {
-        this.Items = [...this.Items.slice(0, index), ...this.Items.slice(index + 1)];
+        items.value = [...items.value.slice(0, index), ...items.value.slice(index + 1)];
       }
-    },
-  },
+  };
+
+  return { addItem, deleteItem, getItems, getTest };
 });
+
 
