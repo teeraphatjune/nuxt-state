707a8c7b3b7d126ff0bdeddbbeff890200d35dfc
teeraphatjune <69952231+teeraphatjune@users.noreply.github.com>
2023-02-03 09:58:20 +0700

    Update DynamicComponent.vue

--- components/DynamicComponent.vue
+++ components/DynamicComponent.vue
@@ -1,19 +1,22 @@
 <template>
-  <component :is="currentComponent" :key="1"/>
+    <component :is="currentComponent" :key="1"/>
 </template>
-
-<script>
-export default defineComponent({
-  data() {
-    return {
-      currentComponent: null,
-    };
-  },
-  mounted() {
-    const componentName = "FormDivide";
-    this.currentComponent = defineAsyncComponent(() =>
-      import(`./${componentName}.vue`)
-    );
-  },
-});
-</script>
+  
+  <script lang="ts">
+  
+  export default {
+    setup() {
+      const currentComponent = shallowRef(null);
+  
+      onMounted(async () => {
+        // API or Something to get components name
+        const componentName = "FormDivide";
+        currentComponent.value = (await import(`./${componentName}.vue`)).default;
+      });
+  
+      return {
+        currentComponent,
+      };
+    },
+  };
+  </script>
